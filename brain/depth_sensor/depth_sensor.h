b94be45603c2951e90deea73187ee4252b9d2147
Felipe Astroza <felipe@astroza.cl>
2014-01-14 21:34:06 +0000

    depth_sensor updates

---
+++ brain/depth_sensor/depth_sensor.h
@@ -0,0 +1,12 @@
+#ifndef DEPTH_SENSOR
+#define DEPTH_SENSOR
+
+int depth_sensor_open();
+unsigned int depth_sensor_read();
+
+#define DS_SAFE_VALUE 500
+#define DS_BORDER_VALUE 280
+
+#define DS_ROBOT_IS_SAFE(value) (value >= DS_SAFE_VALUE)
+
+#endif
